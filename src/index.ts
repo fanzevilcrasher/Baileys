ea500639a1efe55cacb4ff17f8c9709e70f23e0a
fanzevilcrasher <akunwadtikaps@gmail.com>
2025-12-17 10:31:47 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-// --- Tambahkan Kode Kamu di Bagian Paling Atas ---
+// --- BAGIAN LOG ---
 console.log('\x1b[36m%s\x1b[0m', '---------------------------------------------------');
 console.log('\x1b[33m%s\x1b[0m', '🔥 Menggunakan Baileys Custom by Fanzevilcrasher 🔥');
 console.log('\x1b[33m%s\x1b[0m', '   Terima kasih sudah menggunakan script ini!');
 console.log('\x1b[36m%s\x1b[0m', '---------------------------------------------------');
-// -------------------------------------------------
+// ------------------
 
 import makeWASocket from './Socket'
 
@@ -11,7 +11,9 @@ export * from './Socket'
 export * from './Types'
 export * from './Utils'
 export * from './Defaults'
-export * from './WAProto'
 export * from './WAM'
+
+// KITA HAPUS BARIS WAProto YANG BIKIN ERROR
+// export * from './WAProto' <-- Ini biang keroknya kalau filenya gak ada
 
 export default makeWASocket
