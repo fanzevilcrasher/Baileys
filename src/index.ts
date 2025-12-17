22aee28c8d6cc533f33f5b7eb32cf228112ed41d
fanzevilcrasher <akunwadtikaps@gmail.com>
2025-12-17 10:20:11 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import makeWASocket from './Socket/index'
+// --- Tambahkan Kode Kamu di Bagian Paling Atas ---
+console.log('\x1b[36m%s\x1b[0m', '---------------------------------------------------');
+console.log('\x1b[33m%s\x1b[0m', '🔥 Menggunakan Baileys Custom by Fanzevilcrasher 🔥');
+console.log('\x1b[33m%s\x1b[0m', '   Terima kasih sudah menggunakan script ini!');
+console.log('\x1b[36m%s\x1b[0m', '---------------------------------------------------');
+// -------------------------------------------------
 
-export * from '../WAProto/index.js'
-export * from './Utils/index'
-export * from './Types/index'
-export * from './Defaults/index'
-export * from './WABinary/index'
-export * from './WAM/index'
-export * from './WAUSync/index'
+import makeWASocket from './Socket'
 
-export type WASocket = ReturnType<typeof makeWASocket>
-export { makeWASocket }
+export * from './Socket'
+export * from './Types'
+export * from './Utils'
+export * from './Defaults'
+export * from './WAProto'
+export * from './WAM'
+
 export default makeWASocket
